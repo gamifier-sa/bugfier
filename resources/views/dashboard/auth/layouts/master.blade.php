19545e59c73e4b75ef88c98f2d2a347b9eafdbbd
Abdelrahman0000 <khlyftbdh5@gmail.com>
2024-02-06 17:26:07 +0200

    pending page

--- resources/views/dashboard/auth/layouts/master.blade.php
+++ resources/views/dashboard/auth/layouts/master.blade.php
@@ -11,7 +11,7 @@
     <style>body { background-image: url('{{ asset("dashboard-assets/media/auth/bg10.jpeg") }}'); } [data-theme="dark"] body { background-image: url('{{asset('dashboard-assets/media/auth/bg10-dark.jpeg')}}'); }</style>
     <!--end::Page bg image-->
     <!--begin::Authentication - Sign-in -->
-    <div class="d-flex flex-column flex-lg-row flex-column-fluid">
+    <div class="d-flex flex-column flex-lg-row flex-column-fluid justify-content-center ">
     @include('dashboard.auth.layouts.aside')
 
     @yield('content')
--- resources/views/dashboard/auth/pending_register.blade.php
+++ resources/views/dashboard/auth/pending_register.blade.php
@@ -1,24 +1,28 @@
+
 @extends('dashboard.auth.layouts.master')
 @section('title')
     {{__('Pending Register')}}
 @endsection
-
 @push('styles')
     <link href="{{ asset('dashboard-assets/css/Pending.css') }}" rel="stylesheet" type="text/css"/>
 @endpush
+@section('images')
+    <!--begin::Image-->
+    <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="{{asset('dashboard-assets/media/auth/agency2.png')}}" alt="" />
+    <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="{{asset('dashboard-assets/media/auth/agency2dark.png')}}" alt="" />
+    <!--end::Image-->
+@endsection
 
 @section('titleAndText')
-    <!--begin::Title-->
-    <h1 class=" fs-2qx fw-bold text-center mb-7" style='color:var(--kt-gray-400)'>{{__('Well done, you have registered')}}</h1><!--end::Title-->
+   <h1 class=" fs-2qx fw-bold text-center mb-7" style='color:var(--kt-gray-400)'>{{__('Well done, you have registered')}}</h1><!--end::Title-->
 
     <!--begin::Text-->
     <div class=" fs-base text-center fw-semibold" style='color:var(--kt-text-light-300)'>{{__('You have to wait for the admin to approve you')}}
         <br>
-        <a href="{{route('login-form')}}" class="opacity-75-hover text-primary me-1">{{__('To go to the login page')}}</a>
+
+        <a href="{{route('login-form')}}" class="opacity-75-hover text-gold me-1" style='color: var(--kt-warning)'>{{__('TO GO TO THE LOGIN PAGE')}}</a>
     </div><!--end::Text-->
 @endsection
-
-
 
 @push('scripts')
 
