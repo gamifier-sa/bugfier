52008a8cfa93ccf9387378187c6ef3268ea2c362
Abdelrahman0000 <khlyftbdh5@gmail.com>
2024-02-06 17:00:14 +0200

    changes

--- resources/views/dashboard/auth/layouts/aside.blade.php
+++ resources/views/dashboard/auth/layouts/aside.blade.php
@@ -2,7 +2,12 @@
 <div class="d-flex flex-lg-row-fluid">
     <!--begin::Content-->
     <div class="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
-        @yield('images')
+        <!--begin::Image-->
+        <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="{{asset('dashboard-assets/media/auth/agency.png')}}" alt="" />
+        <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="{{asset('dashboard-assets/media/auth/agency-dark.png')}}" alt="" />
+        <!--end::Image-->
+
         @yield('titleAndText')
+
     </div><!--end::Content-->
 </div><!--begin::Aside-->
--- resources/views/dashboard/auth/pending_register.blade.php
+++ resources/views/dashboard/auth/pending_register.blade.php
@@ -3,20 +3,25 @@
     {{__('Pending Register')}}
 @endsection
 
-@section('images')
-    <!--begin::Image-->
-    <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="{{asset('dashboard-assets/media/auth/agency.png')}}" alt="" />
-    <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="{{asset('dashboard-assets/media/auth/agency-dark.png')}}" alt="" />
-    <!--end::Image-->
-@endsection
+@push('styles')
+    <link href="{{ asset('dashboard-assets/css/Pending.css') }}" rel="stylesheet" type="text/css"/>
+@endpush
 
 @section('titleAndText')
     <!--begin::Title-->
-    <h1 class="text-gray-800 fs-2qx fw-bold text-center mb-7">{{__('Well done, you have registered')}}</h1><!--end::Title-->
+    <h1 class=" fs-2qx fw-bold text-center mb-7" style='color:var(--kt-gray-400)'>{{__('Well done, you have registered')}}</h1><!--end::Title-->
 
     <!--begin::Text-->
-    <div class="text-gray-600 fs-base text-center fw-semibold">{{__('You have to wait for the admin to approve you')}}
+    <div class=" fs-base text-center fw-semibold" style='color:var(--kt-text-light-300)'>{{__('You have to wait for the admin to approve you')}}
         <br>
         <a href="{{route('login-form')}}" class="opacity-75-hover text-primary me-1">{{__('To go to the login page')}}</a>
     </div><!--end::Text-->
 @endsection
+
+
+
+@push('scripts')
+
+ <link href="{{ asset('dashboard-assets/css/Pending.css') }}" rel="stylesheet" type="text/css"/>
+
+@endpush
