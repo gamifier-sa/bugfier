b91a3a3512b8f4e7995bae24550a106d973d4f9a
Baraa <baraa_official@outlook.com>
2024-02-06 16:48:05 +0200

    Some Updates

--- resources/views/dashboard/auth/layouts/aside.blade.php
+++ resources/views/dashboard/auth/layouts/aside.blade.php
@@ -2,12 +2,6 @@
 <div class="d-flex flex-lg-row-fluid">
     <!--begin::Content-->
     <div class="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
-        <!--begin::Image-->
-        <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="{{asset('dashboard-assets/media/auth/agency.png')}}" alt="" />
-        <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="{{asset('dashboard-assets/media/auth/agency-dark.png')}}" alt="" />
-        <!--end::Image-->
-
-        @yield('titleAndText')
-
+        @yield('imageAndText')
     </div><!--end::Content-->
 </div><!--begin::Aside-->
--- resources/views/dashboard/auth/pending_register.blade.php
+++ resources/views/dashboard/auth/pending_register.blade.php
@@ -4,6 +4,11 @@
 @endsection
 
 @section('titleAndText')
+    <!--begin::Image-->
+    <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="{{asset('dashboard-assets/media/auth/agency.png')}}" alt="" />
+    <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="{{asset('dashboard-assets/media/auth/agency-dark.png')}}" alt="" />
+    <!--end::Image-->
+
     <!--begin::Title-->
     <h1 class="text-gray-800 fs-2qx fw-bold text-center mb-7">{{__('Well done, you have registered')}}</h1><!--end::Title-->
 
