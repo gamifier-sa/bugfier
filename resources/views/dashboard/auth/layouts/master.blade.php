4c683b1f5e3024ae00605bf0cffc5f4b3ff64b66
Abdelrahman0000 <khlyftbdh5@gmail.com>
2024-02-13 20:26:37 +0200

    signup and signin pages

--- resources/views/dashboard/auth/layouts/master.blade.php
+++ resources/views/dashboard/auth/layouts/master.blade.php
@@ -1,31 +1,42 @@
 <!DOCTYPE html>
-<html lang="{{ getLocale() }}" direction="{{ isArabic() ? 'rtl' : 'ltr' }}" style="direction:{{ getLocale() == 'ar' ? 'rtl' : 'ltr' }}">
+<html lang="{{ getLocale() }}" direction="{{ isArabic() ? 'rtl' : 'ltr' }}"
+    style="direction:{{ getLocale() == 'ar' ? 'rtl' : 'ltr' }}">
 <!--begin::Head-->
 @include('dashboard.auth.layouts.head')
 <!--end::Head-->
 <!--begin::Body-->
+
 <body class="bg-dark">
 
-<div class="d-flex flex-column flex-root">
-    <!--begin::Page bg image-->
-    <style>body { background-image: url('{{ asset("dashboard-assets/media/auth/bg10.jpeg") }}'); } [data-theme="dark"] body { background-image: url('{{asset('dashboard-assets/media/auth/bg10-dark.jpeg')}}'); }</style>
-    <!--end::Page bg image-->
-    <!--begin::Authentication - Sign-in -->
-    <div class="d-flex flex-column flex-lg-row flex-column-fluid justify-content-center ">
-    @include('dashboard.auth.layouts.aside')
+    <div class="d-flex flex-column flex-root">
+        <!--begin::Page bg image-->
+        <style>
+            body {
+                background-image: url('{{ asset('dashboard-assets/media/auth/bg10.jpeg') }}');
+            }
 
-    @yield('content')
+            [data-theme="dark"] body {
+                background-image: url('{{ asset('dashboard-assets/media/auth/bg10-dark.jpeg') }}');
+            }
+        </style>
+        <!--end::Page bg image-->
+        <!--begin::Authentication - Sign-in -->
+        <div class="d-flex flex-column flex-lg-row flex-column-fluid main-container justify-content-center ">
+            @include('dashboard.auth.layouts.aside')
+
+            @yield('content')
 
 
 
+        </div>
+        <!--end::Authentication - Sign-in-->
     </div>
-    <!--end::Authentication - Sign-in-->
-</div>
 
-<!--begin::Javascript-->
-@include('dashboard.auth.layouts.foot')
-<!--end::Javascript-->
+    <!--begin::Javascript-->
+    @include('dashboard.auth.layouts.foot')
+    <!--end::Javascript-->
 
 </body>
 <!--end::Body-->
+
 </html>
